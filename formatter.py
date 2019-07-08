17a0fcdcf56be8eafdecf08e09ae3a36121f850c
Nick Ding <nding1@witsc02n70v2g3q.wks.jnj.com>
2019-07-08 17:02:05 -0400

    able to interpret basic paragraphs and break marks. Working on ul/li interpretation next

---
+++ formatter.py
@@ -0,0 +1,36 @@
+import argparse
+
+# parsing file into command line
+parser = argparse.ArgumentParser()
+parser.add_argument("isi")
+args = parser.parse_args()
+
+
+keywords = "important safety information, contraindications, warnings and precautions, drug interactions"
+
+
+f = open(args.isi, "r")
+
+# putting lines of isi in array
+isiLines = [lines.replace('®', '<sup>®</sup>') for lines in f]
+
+output = ""
+
+# filtering out newline breaks w/ breaks from end of file
+for x in isiLines:
+
+    if(x[0] == '•' or x[0] == '-' or x[0] == '*'):
+        print("BULLET")
+
+
+    if '\n' in x and len(x) > 1:
+        x=x[:-1]
+        output += ("<p>" + x + "</p>\n")
+    elif '\n' in x and len(x) == 1:
+        output += "<br>\n"
+    else:
+        output += "<p>" + x + "</p>"
+
+    print(x)
+
+# print(output)
