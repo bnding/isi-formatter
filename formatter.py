91aba553e9a45aa92ad94ce975f67c6e69412a2b
Nick Ding <nding1@witsc02n70v2g3q.wks.jnj.com>
2019-07-09 12:39:13 -0400

    can interpret ul, li

--- formatter.py
+++ formatter.py
@@ -1,39 +1,87 @@
 import argparse
 
-# parsing file into command line
-parser = argparse.ArgumentParser()
-parser.add_argument("isi")
-args = parser.parse_args()
+keywords = ["important safety information", "contraindications",
+            "warnings and precautions", "drug interactions"]
 
 
-keywords = "important safety information, contraindications, warnings and precautions, drug interactions"
+def getFile():
+
+    # parsing file into command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("isi")
+    args = parser.parse_args()
+    return args.isi
 
 
-f = open(args.isi, "r")
+def fileToList(fileName):
 
-# putting lines of isi in array
-isiLines = [lines.replace('®', '<sup>®</sup>') for lines in f]
-
-output = ""
-
-# filtering out newline breaks w/ breaks from end of file
-for x in isiLines:
-
-    # TODO: Start working on ul/li interpretation
-    if(x[0] == '•' or x[0] == '-' or x[0] == '*'):
-
-        output += "<ul><li>"
-        print("BULLET")
+    f = open(fileName, "r")
+    isiLines = [lines.replace('®', '<sup>®</sup>') for lines in f]
+    f.close()
+    return isiLines
 
 
-    if '\n' in x and len(x) > 1:
-        x=x[:-1]
-        output += ("<p>" + x + "</p>\n")
-    elif '\n' in x and len(x) == 1:
-        output += "<br>\n"
-    else:
-        output += "<p>" + x + "</p>"
+def toHtml(isiLines):
+    output = ""
+    stack = []
 
-    print(x)
+    listState = False
+    for x in isiLines:
+        isList = x[0] == '•' or x[0] == '-' or x[0] == '*'
 
-# print(output)
+        # Checks if the current line is a list
+        if(isList == True):
+            # Checks if a <ul> needs to be added
+            if listState == False:
+                listState = True
+                x = x[1:].strip()
+                output += "<ul><li>" + x + "</li>\n"
+                # stack is implemented to make sure </ul> is written if a list is that last element of list
+                stack.append("<ul>")
+                continue
+
+            elif listState == True:
+                # Middle lists
+                x = x[1:].strip()
+                output += "<li>" + x + "</li>\n"
+                continue
+
+        # Non lists
+        if(isList == False):
+            # Checks for line breaks only or breaks with content
+            if '\n' in x and len(x) > 1:
+                # Check to see if closing ul tag is needed after last li tag
+                if(listState == True):
+                    listState = False
+                    output += "</ul>\n"
+                    stack.pop()
+
+                # \n exists as last character in non-break only lines. Need to remove
+                x = x[:-1]
+                output += ("<p>" + x + "</p>\n")
+            elif '\n' in x and len(x) == 1:
+                output += "<br>\n"
+            else:
+                output += "<p>" + x + "</p>\n"
+
+    # Check for any trailing ul tags
+    while(stack):
+        if(stack[len(stack)-1] == "<ul>"):
+            output += "</ul>"
+        stack.pop()
+
+    return output
+
+
+def main():
+    fileName = getFile()
+    isiLines = fileToList(fileName)
+    htmlIsi = toHtml(isiLines)
+
+    f = open(fileName.replace(".txt", ".html"), "w+")
+    f.write(htmlIsi)
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
